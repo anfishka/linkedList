3f5f45ad51dfc7b094d06845d57bab18a6a4b8ba
anfishka <97487005+anfishka@users.noreply.github.com>
2023-11-21 23:58:09 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,5 @@
-#include <SFML/Graphics.hpp>
 #include <iostream>
-
-
-//1) Создать класс list с свойством head и методом test() : void
-//Который будет схож с main примера
+#include <fstream>
 
 
 class Node 
@@ -13,11 +9,7 @@ private:
     Node* _pNext;
 
 public:
-    Node(int data, Node* pNext)
-    {
-        this->_data = data;
-        this->_pNext = pNext;
-    }
+    Node(int data, Node* pNext = nullptr) : _data(data), _pNext(pNext) {}
 
     int getValue()
     {
@@ -47,6 +39,7 @@ class List
 {
 private:
     Node* _head;
+
 public:
     List() : _head(nullptr) {};
 
@@ -78,6 +71,95 @@ public:
         return current;
     }
 
+    void pushBack(int data)
+    {
+        if (_head == nullptr)
+        {
+            _head = new Node(data);
+        }
+        else
+        {
+            Node* lastNode = getLast();
+            Node* nodeToPush = new Node(data);
+            lastNode->setNextAddr(nodeToPush);
+        }   
+    }
+
+    Node& operator[](const int index)
+    {
+        int counter = 0;
+        Node* current = this->_head;
+        while (current != nullptr)
+        {
+            if (counter == index)
+            {
+                return *current;
+            }
+            current = current->getNextAddr();
+            counter++;
+        }
+        return *_head;
+    }
+
+    void DeleteItem(int index)
+    {
+        if (_head == nullptr || index < 0)
+        {
+            return;
+        }
+        else if (index == 0)
+        {
+            Node* temp = _head;
+            _head = _head->getNextAddr();
+            delete temp;
+            return;
+        }
+        else
+        {
+            Node& previousNode = (*this)[index-1];
+            Node* current = previousNode.getNextAddr();
+
+            if (current == nullptr)
+            {
+                return;
+            }
+            previousNode.setNextAddr(current->getNextAddr());
+            if (current == getLast())
+            {
+                delete current;
+                return;
+            }
+            delete current;
+        }
+    }
+
+    void DeleteLast()
+    {
+        if (_head == nullptr)
+        {
+            return;
+        }
+        else if(_head->getNextAddr()==nullptr)
+        {
+            delete _head;
+            _head = nullptr;
+        }
+        else
+        {
+            Node* current = _head;
+            Node* previous = nullptr;
+
+            while (current->getNextAddr() != nullptr)
+            {
+                previous = current;
+                current = current->getNextAddr();
+            }
+            delete current;
+            previous->setNextAddr(nullptr);
+        }
+
+    }
+
     void test()
     {
         Node* f = new Node(0, nullptr);
@@ -95,15 +177,95 @@ public:
             std::cout << "My addr:" << iter->getAddr() << "\tvalue: " << iter->getValue() << "\tnext address: "<< iter->getNextAddr()<<std::endl;
             iter = iter->getNextAddr();
         }
+
     }
+
+    Node* getHead()
+    {
+        return _head;
+    }
+
+    ~List()
+    {
+        Node* current = _head;
+        while (current != nullptr)
+        {
+            Node* temp = current;
+            current = current->getNextAddr();
+            delete temp;
+        }
+        _head = nullptr;
+    }
+
+    void saveToTxt(std::string& filename)
+    {
+        std::ofstream outputFile(filename);
+        if (!outputFile.is_open())
+        {
+            std::cout << "Unable to open file." << std::endl;
+            return;
+        }
+        Node* current = _head;
+        while (current != nullptr)
+        {
+            outputFile << current->getValue() << "\n";
+            current = current->getNextAddr();
+        }
+        outputFile.close();
+    }
+
+    void readFromTxt(std::string filename)
+    {
+        std::ifstream inputFile(filename);
+        if (!inputFile.is_open())
+        {
+            std::cout << "Unable to open file." << std::endl;
+            return;
+        }
+        int data;
+        while (inputFile >> data)
+        {
+            pushBack(data);
+        }
+        inputFile.close();
+    }
+
+    
+
 };
 
-//2) Сдеать метод getLength() : int для получения количества ячеек списка
-//Сделать метод getLast() : Item * для получения последней ячейки
 
 int main()
 {
     List lst;
+    lst.pushBack(55);
+    lst.pushBack(5580);
+    lst.pushBack(559);
+    lst.pushBack(5455);
+    lst.pushBack(5650);
+    lst.pushBack(550);
+
+    Node* iter = lst.getHead();
+    while (iter)
+    {
+        std::cout << "My addr:" << iter->getAddr() << "\tvalue: " << iter->getValue() << "\tnext address: " << iter->getNextAddr() << std::endl;
+        iter = iter->getNextAddr();
+    }
+
+    std::cout << "\nLast item of the list: " << lst.getLast();
+    std::cout << "\nLength of the list: " << lst.getLength() << std::endl;
+    std::cout << std::endl;
+
+    lst.DeleteItem(2);
+    lst.DeleteLast();
+
+    std::cout << "\nLast item of the list: " << lst.getLast();
+    std::cout << "\nLength of the list: " << lst.getLength() << std::endl;
+    std::cout << std::endl;
+
+    
+
+
     lst.test();
 
     std::cout << "\nLast item of the list: " << lst.getLast();
@@ -111,4 +273,5 @@ int main()
 
     return 0;
 };
+
 
